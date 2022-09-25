2d75f9764e6293a9ed02f413bd96ad998e9d89d9
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-09-25 12:43:36 +0200

    commit fetch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("jeg commiter lige igen ");
         System.out.println("Commit haha");
         System.out.println("test ");
+        System.out.println("Fetch ");
     }
 }

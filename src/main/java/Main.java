3d30d4e66d5cfcd40ca996c365c2c3d23172672f
Mathias-Thomsen <math831t6@stud.kea.dk>
2022-09-25 12:25:50 +0200

    Virker måske

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hallo World!");
         System.out.println("Fuck");
         System.out.println("Virk ordenligt GitHub");
+        System.out.println("Det virker måske nu!");
     }
 }
